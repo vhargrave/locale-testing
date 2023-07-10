1f6027d3d71ad904ef0e05addc44bbb28a189b92
Victor Hargrave <vhargrave@adobe.com>
2023-07-10 14:06:42 +0200

    use hello block

---
+++ blocks/hello/hello.js
@@ -0,0 +1,10 @@
+import { getLibs } from '../../scripts/utils.js';
+
+export default async function init(el) {
+  const link = el.querySelector('a');
+  if (link) {
+    const { createTag } = await import(`${getLibs()}/utils/utils.js`);
+    const img = createTag('img', { src: link.href });
+    el.append(img);
+  }
+}
