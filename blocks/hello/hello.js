ead3b6bfe8808ae66044b409915ea98436535214
Victor Hargrave <vhargrave@adobe.com>
2024-11-13 14:03:05 +0100

    c

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -6,6 +6,6 @@ export default async function init(el) {
   if (link) {
     const { createTag } = await import(`${getLibs()}/utils/utils.js`);
     const img = createTag('img', { src: link.href });
-    el.append(img);
+    el.append(img); //more
   }
 }
