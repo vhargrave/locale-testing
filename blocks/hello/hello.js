7435d25c2815d2b25626bcc4bcfe64c0112be8c3
Victor Hargrave <vhargrave@adobe.com>
2024-11-13 14:01:38 +0100

    remove comment

--- blocks/hello/hello.js
+++ blocks/hello/hello.js
@@ -1,7 +1,6 @@
 import { getLibs } from '../../scripts/utils.js';
 
 export default async function init(el) {
-  // other test
   const link = el.querySelector('a');
   if (link) {
     const { createTag } = await import(`${getLibs()}/utils/utils.js`);
